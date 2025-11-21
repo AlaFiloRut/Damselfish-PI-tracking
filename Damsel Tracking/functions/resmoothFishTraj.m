e2faa99eb6dd5eea60a707174e1605bf5006a688
AlaFiloRut <ala.rutkowska@gmail.com>
2025-11-21 12:32:01 +0000

    Smoothing function uses 'constant' variable

--- Damsel Tracking/functions/resmoothFishTraj.m
+++ Damsel Tracking/functions/resmoothFishTraj.m
@@ -1,6 +1,6 @@
 function traj_sm = resmoothFishTraj(traj,constant)
 nf = length(traj);
-smoothed = csaps(1:nf,traj(:,1:2)',0.1,1:nf)';
+smoothed = csaps(1:nf,traj(:,1:2)',constant,1:nf)';
 smoothDiff = vecnorm(smoothed-traj(:,1:2),2,2);
 spurious = smoothDiff>10;
 smoothed(spurious) = nan;
